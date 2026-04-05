e7959d1ad4d1445166325e5b6a0f12bbe3c78724
Usuario <sofia.hurtado_@uao.edu.co>
2026-04-04 22:17:51 -0500

    aprendiendo herencias en java

---
+++ src/main/java/com/mycompany/herencia830/Herencia830.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herencia830;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Herencia830 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
