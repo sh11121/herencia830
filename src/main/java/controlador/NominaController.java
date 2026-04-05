3432acbb2602abf6f182fcfe0126d66a5e56b91e
Usuario <sofia.hurtado_@uao.edu.co>
2026-04-05 00:07:05 -0500

    creamos el controlador con la sobrecarga de dos metodos, llamados buscar

--- src/main/java/controlador/NominaController.java
+++ src/main/java/controlador/NominaController.java
@@ -8,7 +8,46 @@ package controlador;
  *
  * @author Usuario
  */
+import modelo.Empleado;
+import modelo.Gerente;
+import java.util.ArrayList;
+
 public class NominaController {
 
-    
+    private ArrayList<Empleado> listaEmpleados;
+    //crear atributos
+
+    public NominaController() {
+        this.listaEmpleados = new ArrayList<>();
+    }
+
+    //registro de empleados:
+    public void registrarEmpleados(Empleado e) {
+        listaEmpleados.add(e);//se registra la lista de empleados, se llena de esta forma
+
+    }
+
+    //sobrecarga:
+    public Empleado buscar(String nombre) {
+        for (Empleado e : listaEmpleados) {
+            if (e.getNombre().equalsIgnoreCase(nombre)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+//sobrecarga: buscar por la posicion de la lista:
+    public Empleado buscar(int indice) {
+        if (indice >= 0 && indice < listaEmpleados.size()) {
+            return listaEmpleados.get(indice);
+        }
+        return null;
+    }
+
+    public ArrayList<Empleado> obtenerLista() {
+        return listaEmpleados;
+
+    }
+
 }
